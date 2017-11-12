e9cdee58e617abf0f85eaf3914c5c28d1b9ee72d
mwiesiolek <maksymilian.wiesiolek@gmail.com>
2017-11-12 18:27:15 +0100

    installing redux

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {NgReduxModule, NgRedux} from "ng2-redux";
+import {IAppState, rootReducer} from "./store";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgReduxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(ngRedux: NgRedux<IAppState>) {
+    ngRedux.configureStore(rootReducer, {});
+  }
+}
